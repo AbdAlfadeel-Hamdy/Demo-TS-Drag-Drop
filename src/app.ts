3a3835cdf18bfd567d49fd208d50bce0efa929de
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-22 10:28:56 +0200

    rendering projectLists

--- src/app.ts
+++ src/app.ts
@@ -57,6 +57,42 @@ const Autobind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
   return adjDescriptor;
 };
 
+// ProjectList class
+
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    this.element.querySelector("ul")!.id = `${this.type}-projects-list`;
+    this.element.querySelector(
+      "h2"
+    )!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
 // ProjectInput class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -150,3 +186,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrj = new ProjectList("active");
+const finishedPrrj = new ProjectList("finished");
