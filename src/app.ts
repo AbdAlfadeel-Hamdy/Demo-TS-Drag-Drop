3436134bd83082aa96647b5d3ed333a8f0a985a4
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-22 12:03:51 +0200

    custom types

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
-// Project State Manager
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
 
+// Project State Manager
+type Listener = (projects: Project[]) => void;
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
   private constructor() {}
 
@@ -12,17 +26,19 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random.toString(),
+    const newProject = new Project(
+      Math.random.toString(),
       title,
       description,
-      people: numOfPeople,
-    };
+      numOfPeople,
+      ProjectStatus.Active
+    );
+
     this.projects.push(newProject);
 
     for (const listenerFn of this.listeners) {
@@ -98,7 +114,7 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedprojects: any[];
+  assignedprojects: Project[];
 
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
@@ -116,7 +132,7 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
+    projectState.addListener((projects: Project[]) => {
       this.assignedprojects = projects;
       this.renderProjects();
     });
